a1986d81c48127da6fb1362542ac8e2c7b2e3552
Max Drexel <lord_onager@yahoo.com>
2017-10-16 19:24:03 -0400

    Push train-ride changes

--- JapanesePractice.Debug/Program.cs
+++ JapanesePractice.Debug/Program.cs
@@ -6,8 +6,8 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using JapanesePractice.Contexts;
+using JapanesePractice.Interpretations;
 using JapanesePractice.Loaders;
 
 namespace JapanesePractice.FrontEnd.Debug
@@ -15,6 +15,7 @@ namespace JapanesePractice.FrontEnd.Debug
     public class Program
     {
         private Dictionary<string, ILoader> Loaders;
+        private bool showCount;
 
         public Program(string[] args)
         {
@@ -48,6 +49,9 @@ namespace JapanesePractice.FrontEnd.Debug
             string file = this.GetFileToLoad(root);
             IContext context = this.Loaders["Textual"].LoadContextFromPath(file);
 
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
             this.PrintSeparator();
             List<Category> activeCategories = new List<Category>();
             foreach (Category category in context.Categories)
@@ -66,6 +70,9 @@ namespace JapanesePractice.FrontEnd.Debug
                 throw new InvalidOperationException("No categories selected.");
             }
 
+            Console.Write("For symbols with multiple interpretations, should a count be displayed? (y/n): ");
+            this.showCount = Console.ReadLine().Equals("y", StringComparison.InvariantCultureIgnoreCase);
+
             this.Run(activeCategories);
 
             Console.ReadLine();
@@ -81,33 +88,30 @@ namespace JapanesePractice.FrontEnd.Debug
             {
                 Category category = categories[random.Next(0, categories.Count)];
                 Symbol symbol = category.Symbols[random.Next(0, category.Symbols.Count)];
+                IInterpretation interpretation = symbol.Interpretations[random.Next(0, symbol.Interpretations.Count)];
 
                 List<string> input = new List<string>();
-                while (input.Count != symbol.Interpretations[0].GetPermittedInterpretations().Count())
+                for (int lengthBuffer = interpretation.GetPermittedInterpretations().Count();
+                    input.Count < lengthBuffer;
+                    input.Add(Console.ReadLine()))
                 {
-                    Console.Write(string.Format(
-                        "({0}/{1}) {2}: ",
-                        input.Count + 1,
-                        symbol.Interpretations[0].GetPermittedInterpretations().Count(),
-                        symbol.Name));
-                    input.Add(Console.ReadLine());
+                    this.PromptSymbol(input.Count + 1, lengthBuffer, symbol.Name);
                 }
 
-                if (new Textual.TextualInterpretation(input).CompareAll(symbol.Interpretations))
+                if (interpretation.Compare(new Textual.TextualInterpretation(input)))
                 {
                     correct++;
+                    Console.WriteLine("Correct!");
                 }
-                else
-                {
-                    Console.WriteLine(string.Join(
-                        ", ",
-                        symbol.Interpretations.Select(x =>
-                            string.Format(
-                                "{{{0}}}",
-                                string.Join(
-                                    ", ",
-                                    x.GetPermittedInterpretations().Select(y => y.ToString()))))));
-                }
+
+                Console.WriteLine(string.Join(
+                    ", ",
+                    symbol.Interpretations.Select(x =>
+                        string.Format(
+                            "{{{0}}}",
+                            string.Join(
+                                ", ",
+                                x.GetPermittedInterpretations().Select(y => y.ToString()))))));
 
                 Console.WriteLine(string.Format("Correct: {0}/{1}", correct, ++counter));
             }
@@ -143,6 +147,34 @@ namespace JapanesePractice.FrontEnd.Debug
         private void PrintSeparator()
         {
             Console.WriteLine("================================================================================");
+        }
+
+        private void PrintSpaces(int numberOfSpaces)
+        {
+            // Naive implementation, please don't judge, I know I should write to stream instead.
+            for (int counter = 0; counter < numberOfSpaces; counter++)
+            {
+                Console.Write(' ');
+            }
+        }
+
+        private void PromptSymbol(int currentInterpretationIndex, int totalNumberOfInterpretations, string symbolName)
+        {
+            if (this.showCount)
+            {
+                Console.Write(string.Format(
+                    "({0}/{1}) {2}: ",
+                    currentInterpretationIndex,
+                    totalNumberOfInterpretations,
+                    symbolName));
+            }
+            else
+            {
+                Console.Write(string.Format("{0}: ", symbolName));
+            }
+
+            // Hack for versions of windows prior to the Creators Update (figure out better solution)
+            this.PrintSpaces(symbolName.Length);
         }
     }
 }
