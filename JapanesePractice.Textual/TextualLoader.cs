b6cf1d20ad50bd790da3a4263568cd7b55c5f041
ltnublet <lord_onager@yahoo.com>
2017-10-24 22:30:00 -0400

    Added xmldoc to TextualLoader

--- JapanesePractice.Textual/TextualLoader.cs
+++ JapanesePractice.Textual/TextualLoader.cs
@@ -10,13 +10,28 @@ using JapanesePractice.Contract.Loaders;
 
 namespace JapanesePractice.Textual
 {
+    /// <summary>
+    /// A concrete <see cref="IContext"/> which can produce <see cref="TextualContext"/> or <see cref="TextualInterpretation"/>s from a data source.
+    /// </summary>
     [Export(typeof(ILoader))]
     public class TextualLoader : ILoader
     {
         private static readonly IReadOnlyCollection<string> SupportedTypes = new string[] { "Textual" };
 
+        /// <summary>
+        /// Represents the set of types <see cref="TextualLoader"/> implementation supports.
+        /// </summary>
         public string[] TypesSupported => TextualLoader.SupportedTypes.ToArray();
 
+        /// <summary>
+        /// Creates a <see cref="Category"/> from the supplied JSON-formatted <see cref="string"/> <paramref name="categoryJson"/>.
+        /// </summary>
+        /// <param name="categoryJson">
+        /// The JSON-formatted data from which to create the <see cref="Category"/>.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Category"/> created from the supplied <see cref="string"/> <paramref name="categoryJson"/>.
+        /// </returns>
         public Category CreateCategoryFromJson(string categoryJson)
         {
             JObject jCategory = JObject.Parse(categoryJson);
@@ -25,6 +40,15 @@ namespace JapanesePractice.Textual
                 jCategory.Value<JArray>("Symbols").Select(x => this.CreateSymbolFromJson(x.ToString())));
         }
 
+        /// <summary>
+        /// Creates a <see cref="Symbol"/> from the supplied JSON-formatted <see cref="string"/> <paramref name="symbolJson"/>.
+        /// </summary>
+        /// <param name="symbolJson">
+        /// The JSON-formatted data from which to create the <see cref="Symbol"/>.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Symbol"/> created from the supplied <see cref="string"/> <paramref name="symbolJson"/>.
+        /// </returns>
         public Symbol CreateSymbolFromJson(string symbolJson)
         {
             JObject jSymbol = JObject.Parse(symbolJson);
@@ -33,6 +57,15 @@ namespace JapanesePractice.Textual
                 new TextualInterpretation(jSymbol.Value<JArray>("Interpretations").Select(x => x.ToString())));
         }
 
+        /// <summary>
+        /// Creates a <see cref="TextualContext"/> from the file specified by <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">
+        /// The path the <see cref="TextualLoader"/> should create the <see cref="TextualContext"/> from.
+        /// </param>
+        /// <returns>
+        /// An <see cref="TextualContext"/> created by the <see cref="TextualLoader"/>.
+        /// </returns>
         [System.Diagnostics.CodeAnalysis.SuppressMessage(
             "Microsoft.Usage",
             "CA2202:Do not dispose objects multiple times",
