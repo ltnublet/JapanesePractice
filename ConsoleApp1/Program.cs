54203528eab7a489b466bf5e87fcbea1a160b2dc
ltnublet <lord_onager@yahoo.com>
2017-11-12 22:45:35 -0500

    Trying to find out why MEF is broken only in main project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using JapanesePractice.Contract.Loaders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        [ImportMany(typeof(ILoader))]
+        private ILoader[] loaders = new ILoader[0];
+
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+
+            Console.ReadLine();
+        }
+
+        public void Run()
+        {
+            using (AggregateCatalog catalog = new AggregateCatalog())
+            {
+                string location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                Console.WriteLine(location);
+                catalog.Catalogs.Add(new DirectoryCatalog(location));
+
+                using (CompositionContainer container = new CompositionContainer(catalog))
+                {
+                    container.ComposeParts(this);
+                }
+            }
+
+            foreach (ILoader loader in this.loaders)
+            {
+                Console.WriteLine(loader.ToString());
+            }
+        }
+    }
+}
