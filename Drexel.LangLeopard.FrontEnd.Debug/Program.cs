c78b6e015568aab24817e72ae93e70d938b4a472
user <ltnublet@users.noreply.github.com>
2018-06-21 23:35:50 -0400

    Pushing the magical combination of references that lets you use a RegistrationBuilder (need to have a reference to System.Reflection.Context)

---
+++ Drexel.LangLeopard.FrontEnd.Debug/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.ReflectionModel;
+using System.ComponentModel.Composition.Registration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drexel.LangLeopard.FrontEnd.Debug
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RegistrationBuilder builder = new RegistrationBuilder();
+
+
+            string directory = Directory.GetCurrentDirectory();
+            AssemblyCatalog[] assemblyCatalogs = new DirectoryCatalog(directory)
+                .Select(x => ReflectionModelServices.GetPartType(x).Value.Assembly)
+                .Distinct()
+                .Select(x => new AssemblyCatalog(x, reflectionContext: builder))
+                .ToArray();
+
+            AggregateCatalog catalog = new AggregateCatalog(assemblyCatalogs);
+            CompositionContainer container = new CompositionContainer(catalog);
+        }
+    }
+}
